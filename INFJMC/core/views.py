0bf2d2aca7b244b1b0ffe86cef9f39b80acf4f7d
sebastiantang <sebastian.tang@usm.cl>
2022-10-17 16:29:57 -0300

    avance

--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -3,15 +3,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
+menu = "<hr><ul>"
+menu +=" <li><a href='http://127.0.0.1:8000/'>Inicio</a></li>"
+menu +=" <li><a href='http://127.0.0.1:8000/carreras/'>Carreras</a></li>"
+menu +=" <li><a href='http://127.0.0.1:8000/docentes/'>Docentes</a></li>"
+menu +="</ul>"
+
+contenido = "<hr><p> lorem. </p>"
 # Create your views here.
 def home(request):
-    titulo = "<h1>Informatica USM</h1> <hr> <a href='http://127.0.0.1:8000/carreras/'>Carreras</a> <a href='http://127.0.0.1:8000/docentes/'>Docentes</a> <hr>"
-    return HttpResponse(titulo)
+    titulo = "<h1>Informatica USM</h1>"
+    html = titulo+menu+contenido
+    return HttpResponse(html)
 
 def carreras(request):
-    titulo = "<h1>Carreras</h1> <hr> <a href='http://127.0.0.1:8000/'>Home</a> <a href='http://127.0.0.1:8000/docentes/'>Docentes</a>"
-    return HttpResponse(titulo)
+    titulo = "<h1>Carreras</h1>"
+    html = titulo+menu
+    return HttpResponse(html)
 
 def docentes(request):
-    titulo = "<h1>Docentes</h1><hr> <a href='http://127.0.0.1:8000/'>Home</a> <a href='http://127.0.0.1:8000/carreras/'>Carreras</a>"
-    return HttpResponse(titulo)
+    titulo = "<h1>Docentes</h1>"
+    html = titulo+menu
+    return HttpResponse(html)
